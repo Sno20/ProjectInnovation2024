01ad5f0253cada2d78d3e9f3e851ac79ce273f68
morqp <mara.cosneanu@yahoo.com>
2024-02-15 21:45:51 +0100

    gyro

--- ProjectInnovation2024/Assets/Scenes/Mechanics/Accelorometer/Accelorometer.cs
+++ ProjectInnovation2024/Assets/Scenes/Mechanics/Accelorometer/Accelorometer.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Accelorometer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public Sprite redBeaker;
+    public Sprite purpleBeaker;
+
+    Vector3 accelerometer;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        accelerometer = Input.acceleration;
+
+        if(accelerometer.sqrMagnitude > 5f)
+        {
+            Debug.Log("shakey shake");
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = purpleBeaker;
+
+        }
     }
+
 }

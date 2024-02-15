926d2b9ea5f6dc3efd426f6b4cca0900e6233fbf
Emily74x <emilymurad2016@gmail.com>
2024-02-15 21:44:54 +0100

    Gyroscope script pour
    Main functionality using phone's Z

--- ProjectInnovation2024/Assets/Scenes/Mechanics/GyroScope/GyroScope.cs
+++ ProjectInnovation2024/Assets/Scenes/Mechanics/GyroScope/GyroScope.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GyroScope : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Gyroscope : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-    
+  private void Start() {
+    if (SystemInfo.supportsGyroscope) { //check if device has gyroscope
+      Input.gyro.enabled = true; //enable use of gyroscope
     }
+    else {
+      Debug.Log("Gyroscope not supported"); //message if not supported
+    }
+  }
+
+  private void Update() {
+    SetZ();
+    CheckZ();
+  }
+
+  private void SetZ() {
+    Vector3 gyroRot2D = new Vector3(0, 0, Input.gyro.attitude.eulerAngles.z); //store gyroscope z in a vec3
+    transform.rotation = Quaternion.Euler(gyroRot2D); //set obj rotation z to gyroscope rotation z) 
+  }
+
+  private void CheckZ() {
+    if (transform.rotation.eulerAngles.z > 90 && transform.rotation.eulerAngles.z < 180) {
+      Debug.Log("Pouring now");
+    }
+  }
 }

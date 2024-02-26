60cd6d57513a1cd203e4175829ac826b2aa1d51f
Emily74x <emilymurad2016@gmail.com>
2024-02-26 16:41:20 +0100

    Calibration added for gyroscope
    Fix the odd inconsistent behaviour

--- ProjectInnovation2024/Assets/Scenes/Mechanics/GyroScope/PourPotion/PourPotion.cs
+++ ProjectInnovation2024/Assets/Scenes/Mechanics/GyroScope/PourPotion/PourPotion.cs
@@ -2,20 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 public class PourPotion : MonoBehaviour {
 
-  [SerializeField] private float minPhoneRotationX = 30;
-  [SerializeField] private float maxPhoneRotationX = 70;
-  [SerializeField] private float minPourAngleX = 120;
-  [SerializeField] private float maxPourAngleX = 180;
+  [SerializeField] private float minPhoneRotationX = 340f;
+  [SerializeField] private float maxPhoneRotationX = 20f;
+  [SerializeField] private float minPourAngleX = 0;
+  [SerializeField] private float maxPourAngleX = 0;
 
   [SerializeField] private TextMeshProUGUI textBox; //for testing
   [SerializeField] private bool upright = true; //for testing
 
+  private Quaternion initialOrientation;
+  private bool isCalibrated = false;
+
   private void Start() {
     if (SystemInfo.supportsGyroscope) { //check if device has gyroscope
       Input.gyro.enabled = true; //enable use of gyroscope
+      CalibrateGyro();
     }
     else {
       Debug.Log("Gyroscope not supported"); //message if not supported
@@ -23,24 +28,38 @@ public class PourPotion : MonoBehaviour {
   }
 
   private void Update() {
+    if (!isCalibrated) {
+      return;
+    }
+
     GyroCheck();
   }
 
+  private void CalibrateGyro() {
+    // Capture the initial orientation as the inverse of the current attitude
+    // This makes the current orientation the "neutral" or reference point
+    initialOrientation = Quaternion.Inverse(Input.gyro.attitude);
+    isCalibrated = true;
+  }
+
+  //Optionally, expose this method to allow recalibration from the UI
+  public void Recalibrate() {
+    CalibrateGyro();
+  }
+
+  Vector3 spriteRotation;
   private void GyroCheck() {
-    Vector3 gyroRot = new Vector3(Input.gyro.attitude.eulerAngles.x, Input.gyro.attitude.eulerAngles.y, Input.gyro.attitude.eulerAngles.z); //set gyro input to vector3
-    Vector3 spriteRotation = new Vector3(0, 0, gyroRot.z); //set sprite rotationZ to gyroZ
-    if (gyroRot.x > minPhoneRotationX && gyroRot.x < maxPhoneRotationX) { //check if phone is upright within minimum and maximum values
-      transform.rotation = Quaternion.Euler(spriteRotation); //set sprite rotationZ to vector3 spriteRotation
-      if (gyroRot.z > minPourAngleX && gyroRot.z < maxPourAngleX) { //check if phone is doing pouring motion within minimum and maximum values
-        Pour(); 
-      }
-      else {
-        textBox.text = "Now pour the potion";
-      }
+    // Apply the calibration offset to the current orientation
+    Quaternion correctedOrientation = Input.gyro.attitude * initialOrientation;
+    Vector3 gyroRot = correctedOrientation.eulerAngles; // Use corrected orientation
+
+    
+    if (gyroRot.x > minPhoneRotationX || gyroRot.x < maxPhoneRotationX) {
+      Debug.Log("in range");
+      spriteRotation = new Vector3(0, 0, -gyroRot.y);
+      transform.rotation = Quaternion.Euler(spriteRotation);
     }
-    else {
-      if (upright) textBox.text = "Hold your phone upright";
-    }
+    textBox.text = gyroRot.ToString() + "\n" + minPhoneRotationX + "\n" + maxPhoneRotationX + "\n" + spriteRotation.ToString();
   }
 
   private void Pour() {

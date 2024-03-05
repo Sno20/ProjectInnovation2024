4292380c75dffed7c0284f1f653be41378d720f4
Emily74x <emilymurad2016@gmail.com>
2024-03-05 16:11:59 +0100

    IP as code works
    Changes in PC listener and Phone sender

--- ProjectInnovation2024/Assets/NoGlitch/UDPListener.cs
+++ ProjectInnovation2024/Assets/NoGlitch/UDPListener.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using TMPro;
 
 public class UDPListener : MonoBehaviour {
 
@@ -13,13 +14,29 @@ public class UDPListener : MonoBehaviour {
   public Quaternion gyroQuaternion;
   public float accelerationSqrMagnitude;
 
+  [SerializeField] private TextMeshProUGUI ipv4;
+  private string currentIP;
+
   private void Start() {
     client = new UdpClient(8089);
     endPoint = new IPEndPoint(IPAddress.Any, 0);
+    LogLocalIPAddress();
   }
 
   private void Update() {
     ReceiveData();
+  }
+
+  private void LogLocalIPAddress() {
+    var host = Dns.GetHostEntry(Dns.GetHostName());
+    foreach (var ip in host.AddressList) {
+      if (ip.AddressFamily == AddressFamily.InterNetwork) {
+        //Debug.Log("Local IPv4: " + ip.ToString());
+        currentIP = ip.ToString();
+        ipv4.text = ip.ToString();
+        break; // Remove this break if you want to log all IPv4 addresses
+      }
+    }
   }
 
   void ReceiveData() {
@@ -28,7 +45,14 @@ public class UDPListener : MonoBehaviour {
       string inString = Encoding.UTF8.GetString(inBytes);
       
       if (inString.StartsWith("IP:")) {
-        //Debug.Log($"Received IP: {inString.Substring(3)} from {endPoint}");
+        Debug.Log($"Received IP: {inString.Substring(3)} from {endPoint}");
+        string receivedIP = inString.Substring(3).Trim();
+        if (receivedIP == currentIP) {
+          ipv4.text = " ";
+        }
+        else {
+          Debug.Log("Try again");
+        }
       }
       else if (inString.StartsWith("GYRO:")) {
         string gyroData = inString.Substring(5);
@@ -55,7 +79,7 @@ public class UDPListener : MonoBehaviour {
     return Quaternion.identity;
   }
 
-  private void OnDestroy() {
+    private void OnDestroy() {
     if (client != null)
       client.Close();
   }

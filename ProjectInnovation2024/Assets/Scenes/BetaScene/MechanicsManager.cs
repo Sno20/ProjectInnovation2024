96fcb5d3f15677b5f970217fcd5373f2474b58cd
Emily74x <emilymurad2016@gmail.com>
2024-03-08 01:56:52 +0100

    Fixed script merge conflict

--- ProjectInnovation2024/Assets/Scenes/BetaScene/MechanicsManager.cs
+++ ProjectInnovation2024/Assets/Scenes/BetaScene/MechanicsManager.cs
@@ -17,13 +17,12 @@ public class MechanicsManager : MonoBehaviour {
   [SerializeField] private GameObject potionToPour;
 
 
-    [SerializeField] private GameObject roomBrewery;
-    [SerializeField] private GameObject roomKey;
+  [SerializeField] private GameObject roomBrewery;
+  [SerializeField] private GameObject roomKey;
 
 
-    private void Start()
-    {
-    }
+  private void Start() {
+  }
 
   private void Update() {
 
@@ -40,98 +39,98 @@ public class MechanicsManager : MonoBehaviour {
     {
         interactedWithUV = true;
     }*/
+  }
+
+  void SwitchMechanics(int room) {
 
 
+    /*else if (room != 0) {
+      mechanics[0].SetActive(false);
+    }*/
 
-    }
-    else if (room != 0) {
+    if (room != 0) {
       mechanics[0].SetActive(false);
     }
-    ////////////
-    if (room == 1)  //  room 2 throw
+      
+      ////////////
+      if (room == 1)  //  room 2 throw
     {
-        ////////////
-        if (room == 0)  //  room 0 UV
-        {
-            mechanics[0].SetActive(true);
-        }
-        else if (room != 0)
-        {
-            mechanics[0].SetActive(false);
-        }
-        ////////////
-        if (room == 1)  //  room 1 throw
-        {
+      ////////////
+      if (room == 0)  //  room 0 UV
+      {
+        mechanics[0].SetActive(true);
+      }
+      else if (room != 0) {
+        mechanics[0].SetActive(false);
+      }
+      ////////////
+      if (room == 1)  //  room 1 throw
+      {
 
-            if (mechanics[3].GetComponent<MixUDP>().check) //activate throw
-            {
-                mechanics[4].SetActive(true);
-            }
-            if (mechanics[4].GetComponentInChildren<ThrowUDP>().explosion) // after thrown activate door
-            {
-                mechanics[5].SetActive(true);
-            }
-        }
-        else if (room != 1)
+        if (mechanics[3].GetComponent<MixUDP>().check) //activate throw
         {
-            mechanics[4].SetActive(false);
-            mechanics[5].SetActive(false);
+          mechanics[4].SetActive(true);
         }
-        ////////////
-        if (room == 2)  //  room 2  pour & mix potion
+        if (mechanics[4].GetComponentInChildren<ThrowUDP>().explosion) // after thrown activate door
         {
-            mechanics[2].SetActive(true);
-            /*mechanics[3].SetActive(true);
-            if (mechanics[1].GetComponentInChildren<HammerUDP>().potion) // if we took the potion
-            {
-                potionToPour.SetActive(true); // set to true the vessel wuth mix in it
-            }*/
+          mechanics[5].SetActive(true);
         }
-        else if (room != 2)
+      }
+      else if (room != 1) {
+        mechanics[4].SetActive(false);
+        mechanics[5].SetActive(false);
+      }
+      ////////////
+      if (room == 2)  //  room 2  pour & mix potion
+      {
+        mechanics[2].SetActive(true);
+        /*mechanics[3].SetActive(true);
+        if (mechanics[1].GetComponentInChildren<HammerUDP>().potion) // if we took the potion
         {
-            mechanics[2].SetActive(false);
-            //mechanics[3].SetActive(false);
-        }
-        ////////////
-        if (room == 3)  //  room 3 hammer and pick up potion
-        {
-            mechanics[1].SetActive(true);
+            potionToPour.SetActive(true); // set to true the vessel wuth mix in it
+        }*/
+      }
+      else if (room != 2) {
+        mechanics[2].SetActive(false);
+        //mechanics[3].SetActive(false);
+      }
+      ////////////
+      if (room == 3)  //  room 3 hammer and pick up potion
+      {
+        mechanics[1].SetActive(true);
 
-        }
-        else if (room != 3)
-        {
-            mechanics[1].SetActive(false);
-        }
-        ////////////
-        if (roomBrewery.activeInHierarchy)  //      zoom in room 2 
-        {
-            Debug.Log("active? " + roomBrewery.activeInHierarchy);
-            mechanics[6].SetActive(true);
+      }
+      else if (room != 3) {
+        mechanics[1].SetActive(false);
+      }
+      ////////////
+      if (roomBrewery.activeInHierarchy)  //      zoom in room 2 
+      {
+        Debug.Log("active? " + roomBrewery.activeInHierarchy);
+        mechanics[6].SetActive(true);
 
-            if (mechanics[1].GetComponentInChildren<HammerUDP>().potion) // if we took the potion
-            {
-                potionToPour.SetActive(true); // set to true the vessel wuth mix in it
-            }
-        }
-        else if (!roomBrewery.activeSelf)
+        if (mechanics[1].GetComponentInChildren<HammerUDP>().potion) // if we took the potion
         {
-            mechanics[6].SetActive(false);
+          potionToPour.SetActive(true); // set to true the vessel wuth mix in it
+        }
+      }
+      else if (!roomBrewery.activeSelf) {
+        mechanics[6].SetActive(false);
 
-            /* mechanics[2].SetActive(false);
-             mechanics[3].SetActive(false);*/
-        }
-        //////////////
-        if (roomKey.activeInHierarchy)  //      zoom in room 2 
-        {
-            Debug.Log("active? " + roomBrewery.activeInHierarchy);
-            mechanics[7].SetActive(true);
-        }
-        else if (!roomBrewery.activeSelf)
-        {
-            mechanics[7].SetActive(false);
+        /* mechanics[2].SetActive(false);
+         mechanics[3].SetActive(false);*/
+      }
+      //////////////
+      if (roomKey.activeInHierarchy)  //      zoom in room 2 
+      {
+        Debug.Log("active? " + roomBrewery.activeInHierarchy);
+        mechanics[7].SetActive(true);
+      }
+      else if (!roomBrewery.activeSelf) {
+        mechanics[7].SetActive(false);
 
-           
-        }
+
+      }
 
 
     }
@@ -141,8 +140,6 @@ public class MechanicsManager : MonoBehaviour {
       /* mechanics[2].SetActive(false);
        mechanics[3].SetActive(false);*/
     }
-
-
   }
 
   public void InteractedWithUV() {
